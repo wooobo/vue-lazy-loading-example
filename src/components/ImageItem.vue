40f3fe4a833bcb112f400270dfbab1b63fee135d
강태화 <aogora2431@gmail.com>
2019-08-30 01:55:25 +0900

    commit

---
+++ src/components/ImageItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="img-item">
+      <!-- <img :src="imgSrc" v-img-lazy-loading />  전달 받은 이미지 URL을 바인딩 -->
+      <div class="conatiner" v-img-lazy-loading="imgSrc"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageItem',
+  props: {
+      imgSrc: String, // 부모 컴포넌트 이미지 URL props를 전달 받기위해 추가합니다
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .conatiner {
+    position: relative;
+    width: 600px;
+    height: 600px;
+    margin-bottom: 10px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+require('./directives'); // directives 모듈 추가
+
 Vue.config.productionTip = false
 
 new Vue({
